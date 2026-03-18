1e9c34c986b20c02f37d99cbdead4a7f53cd3e0b
Ali-Hajipour <alihajipour.it@gmail.com>
2026-03-18 02:26:42 +0100

    ports enrty limit in main.py added

--- main.py
+++ main.py
@@ -5,5 +5,29 @@ import os
 from scanner import ful_scan , COMMON_SERVICE_PORTS
 from reporter import print_summary , save_report
 
+def parse_ports(port_string : str):
+
+
+    if port_string.lower() == "common" :
+        return list(COMMON_SERVICE_PORTS.keys())
+
+
+    ports = set()
+
+
+    for part in port_string.split(","):
+        part = part.strip()
+        if "-" in part:
+            try :
+                start , end = part.split("-" , 1)
+                start ,end = int(start.strip()) , int(end.strip())
+                if start > end or end >65535:
+                    raise ValueError
+                ports.update(range(start, end+1))
+            except ValueError :
+                print(f"Invalid Port : {part} must be in range of 1 to 65535")
+                sys.exit(1)
+
+    return sorted(ports)
 
 
